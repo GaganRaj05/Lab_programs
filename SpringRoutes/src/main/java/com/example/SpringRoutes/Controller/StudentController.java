ab567307de8fda305cf5da4ed57373be19d60568
Lucifer <itzmegagan2@gmail.com>
2024-11-27 02:20:27 +0530

    spring route

---
+++ SpringRoutes/src/main/java/com/example/SpringRoutes/Controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.example.SpringRoutes.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @GetMapping("/get")
+    public String get() {
+        return "hello";
+    }
+}
---
+++ SpringRoutes/src/main/java/com/example/SpringRoutes/SpringRoutesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringRoutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRoutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRoutesApplication.class, args);
+	}
+
+}
