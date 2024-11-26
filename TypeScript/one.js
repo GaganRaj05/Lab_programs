ac2f9cefaceae82a830f013b4ca3580e57bf76e5
Lucifer <itzmegagan2@gmail.com>
2024-11-26 18:58:55 +0530

    one

---
+++ TypeScript/one.js
@@ -0,0 +1,10 @@
+var a = null;
+var b = 55;
+var c = "Gagan";
+var d = 789;
+var e = true;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
---
+++ TypeScript/one.ts
@@ -0,0 +1,10 @@
+let a = null;
+let b :Number = 55;
+let c:String = "Gagan";
+let d:any=789;
+let e:boolean = true;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
